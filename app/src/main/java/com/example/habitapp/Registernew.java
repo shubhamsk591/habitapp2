317514c56c330ddea49655ba577643714a09f902
shubhamsk591 <86595056+shubhamsk591@users.noreply.github.com>
2021-10-27 11:20:50 +0530

    make Registernew with toast

--- app/src/main/java/com/example/habitapp/Registernew.java
+++ app/src/main/java/com/example/habitapp/Registernew.java
@@ -15,9 +15,9 @@ public class Registernew extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registerform);
 
-        EditText username=(EditText)findViewById(R.id.username);
-        EditText password=(EditText)findViewById(R.id.password);
-        Button cancelbutton = (Button) findViewById(R.id.cancel_button);
+        EditText username=(EditText)findViewById(R.id.rname_input);
+        EditText password=(EditText)findViewById(R.id.rpassword_input);
+        Button cancelbutton = (Button) findViewById(R.id.rcancel_button);
         cancelbutton.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -26,12 +26,21 @@ public class Registernew extends AppCompatActivity {
                 startActivity(myintent);
             }
         });
-        Button registerbutton = (Button) findViewById(R.id.register_button);
+        Button registerbutton = (Button) findViewById(R.id.rregister_button);
         registerbutton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick (View v){
-                Toast.makeText(Registernew.this, "R.string.welcome_text"+username, Toast.LENGTH_LONG).show();
+                String msg="Username : "+username.getText().toString()+"\nPassword : "+password.getText().toString();
+                Toast.makeText(Registernew.this,"Welcome to habit and motivation app \n"+msg, Toast.LENGTH_LONG).show();
+            }
+        });
+        Button loginbutton=(Button) findViewById(R.id.rLogin_button);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent =new Intent(view.getContext(), Loginform.class);
+                startActivity(myintent);
             }
         });
 }
